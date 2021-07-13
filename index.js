5d3a8e8ce4561f9220bc0bc37ce66cd39cf51dbc
SD <harrypotter.starwars.shrish@gmail.com>
2021-07-13 15:57:25 +0530

    IT WORKS *dab*

--- index.js
+++ index.js
@@ -1,15 +1,3 @@
-const copyToClipboard = str => {
-  const el = document.createElement('textarea');
-  el.value = str;
-  el.setAttribute('readonly', '');
-  el.style.position = 'absolute';
-  el.style.left = '-9999px';
-  document.body.appendChild(el);
-  el.select();
-  document.execCommand('copy');
-  document.body.removeChild(el);
-};
-
 function pyramidify() {
     let input = document.getElementById("inputtext").value;
     if (input.length == 0) {
@@ -35,5 +23,5 @@ function setTextAreaValue(value) {
 }
 
 function copyOutput() {
-    copyToClipboard(document.getElementById("outputp").innerHTML);
+    navigator.clipboard.writeText(document.getElementById(outputp).value());
 }
