d4f9c845b0ac0afa18363d8ddbe2e8c6043903df
SD <harrypotter.starwars.shrish@gmail.com>
2021-07-13 15:03:41 +0530

    bad code but it works kekw

---
+++ index.js
@@ -0,0 +1,23 @@
+function pyramidify() {
+    let input = document.getElementById("inputtext").value;
+    if (input.length == 0) {
+        setTextAreaValue("");
+        return;
+    }
+    var output = "";
+    for (let i = 1; i < input.length; i++) {
+        output += input.substring(0, i);
+        output += "\n";
+    }
+    for (let i = input.length; i > 0; i--) {
+        output += input.substring(0, i);
+        if (i > 1) {
+            output += "\n";
+        }
+    }
+    setTextAreaValue(output);
+}
+
+function setTextAreaValue(value) {
+    document.getElementById("outputp").innerHTML = value
+}
